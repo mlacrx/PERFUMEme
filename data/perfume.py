7c4108b94bb6a26e6a953f6cbb8c9bda3ba6cfb6
mlacrx <marie.lacroix@epfl.ch>
2025-05-06 11:17:17 +0200

    database of perfume

--- data/perfume.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class perfume:
-    def __init__(self, name, brand, molecules, notes):
-        self.name = name
-        self.brand = brand
-        self.molecules = molecules
-        self.notes = notes
-
